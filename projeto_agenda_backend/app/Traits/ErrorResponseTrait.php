c58fbce30032d130b2b07de31e41075379a7e6d2
FelipeEliasSoares <felipe.soares@enterscience.com.br>
2024-09-26 18:32:30 -0300

    :sparkles: feat: add RegisterPage and  Interface FormRegister

--- projeto_agenda_backend/app/Traits/ErrorResponseTrait.php
+++ projeto_agenda_backend/app/Traits/ErrorResponseTrait.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\DB;
 
 trait ErrorResponseTrait
 {
+    /**
+     * Handles exceptions by rolling back the database transaction and returning a JSON response.
+     *
+     * @param Exception $ex The exception that was thrown.
+     * @param string $message A custom message to include in the response.
+     * @param int $status The HTTP status code to return.
+     * @return \Illuminate\Http\JsonResponse The JSON response containing the error details.
+     */
     protected function handleException(Exception $ex, $message, $status)
     {
         DB::rollback();
